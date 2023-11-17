9ae63db416ad03b42939259ccb5176bbd51ce0f4
akparmar004 <akparmar0404@gmail.com>
2023-11-17 17:56:28 +0530

    string operations

--- fundamental/string.cpp
+++ fundamental/string.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 int main()
 {
-	char s[20];
-	cout << "enter string - ";
+	//char s[20];
+	string s;
 	//cin >> s; //without space
-	cin.getline(s,50); //with space
+	cout << sizeof(s) << endl;
+	cout << "enter string - ";
+	getline(cin,s);
+	//cin.getline(s,50); //with space
 	cout << "string - " << s << endl;
 	return 0;
 }
