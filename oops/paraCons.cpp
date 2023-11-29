c91b67e9fd56352b9f62ed7143ae99c194832ddf
akparmar004 <akparmar0404@gmail.com>
2023-11-29 11:10:51 +0530

    constructor overloading

--- oops/paraCons.cpp
+++ oops/paraCons.cpp
@@ -1,17 +1,26 @@
 #include<iostream>
 using namespace std;
 
+//constructor overloading 
+//constructor with same name and having different numbers of arguments..
 class a
 {
 	int cash;
 	
 	public :
+		a();
 		a(int);
 		void get_data();
 };
 
+a::a()
+{
+	cout << "default cons." << endl;
+	cash = 100;
+}
 a::a(int b)
 {
+	cout << "parameterized cons." << endl;
 	cash = b;
 }
 
@@ -22,6 +31,7 @@ void a::get_data()
 
 int main()
 {
-	a a1(200);
+	a a1,a2(200);
 	a1.get_data();
+	a2.get_data();
 }
